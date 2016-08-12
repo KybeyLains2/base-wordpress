becfed22e80b4bbb21f7c1ae85a2ddcabe3d5877
Walker Franson <walker_franson@hotmail.com>
2016-08-12 15:30:58 -0300

    Ajustado Gruntfile para plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,6 @@ module.exports = function (grunt) {
 							"**",
 							"!*.git",
 							"!**/themes/twenty**/**",
-							"!**/plugins/**/**",
 							'!**/_assets/**'
 						],
 						dest: 'dist/',
