e7d1ce7fa6b85437d525a1796979e474affeb884
Eduardo Barros <eduzial@gmail.com>
2018-06-11 17:04:49 +0000

    Plugin base

---
+++ src/BasicTheme/wp-content/plugins/plugin-base/plugin-base.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: COLOCAR O NOME DO PLUGIN
+ * Plugin URI: http://jawsdigital.com.br
+ * Description: DESCRICAO AQUI
+ * Author: AUTOR
+ * Version: 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Mostra apenas uma tela branca se alguém tentar acessar o arquivo diretamente.
+}
+
+
+
